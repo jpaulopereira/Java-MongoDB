39af9c004755a419262385f56e8920f340aad161
jpaulopereira <joaopaulo.ae@gmail.com>
2024-07-18 08:00:47 -0300

    initial

---
+++ src/test/java/br/com/jotape/ProjetoComMongoDBApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jotape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoComMongoDBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
